57c8a88415b25a393aa4afd261a538468d2a350b
Michael Lieberman <mlieberman85@gmail.com>
2022-03-30 14:35:31 -0400

    Remove errant line
    Issue with copy pasting history, included incorrect line.

--- bootstrap/nixos-install.sh
+++ bootstrap/nixos-install.sh
@@ -9,7 +9,6 @@ parted /dev/nvme0n1 -- mkpart primary 512MiB -8GiB
 parted /dev/nvme0n1 -- mkpart primary linux-swap -8GiB 100%
 parted /dev/nvme0n1 -- mkpart ESP fat32 1MiB 512MiB
 parted /dev/nvme0n1 -- set 3 esp on
-mkfs.ext4 -L nixos /dev/nvme0n1
 mkfs.ext4 -L nixos /dev/nvme0n1p1
 mkswap -L swap /dev/nvme0n1p2
 mkfs.fat -F 32 -n boot /dev/nvme0n1p3
